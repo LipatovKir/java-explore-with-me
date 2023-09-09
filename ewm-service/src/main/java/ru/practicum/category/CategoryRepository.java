1880f6fd8906b35c7ca7bdd7475ab09ab133cd4c
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2023-09-09 16:52:07 +0300

    Diplom part 2 (1-ая итерация)

--- ewm-service/src/main/java/ru/practicum/category/CategoryRepository.java
+++ ewm-service/src/main/java/ru/practicum/category/CategoryRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
