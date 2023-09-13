1c8945ff9f6f18d284b84415f446736a5458a896
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2023-09-13 23:56:01 +0300

    Diplom part 3 (2-ая итерация. Исправить недочеты )

--- stats-service/server/src/main/java/ru/practicum/controller/HitController.java
+++ stats-service/server/src/main/java/ru/practicum/controller/HitController.java
@@ -3,7 +3,6 @@ package ru.practicum.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.HitDto;
 import ru.practicum.dto.StatsDto;
@@ -16,7 +15,6 @@ import java.util.List;
 import static ru.practicum.Constant.FORMATTER;
 
 @Slf4j
-@Validated
 @RestController
 @RequiredArgsConstructor
 public class HitController {
